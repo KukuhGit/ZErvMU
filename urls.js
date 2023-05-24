11b3e21a19131593546061b1b4ae6612f6fa44b7
KukuhGit <97213948+KukuhGit@users.noreply.github.com>
2023-05-24 20:17:22 +0700

    urls.js

---
+++ urls.js
@@ -0,0 +1,23 @@
+  
+    var obj = {
+  Assassin: {
+    Fanny: {
+      Aspirant: "https://google.com",
+      Many:"https://google.com"
+    },
+    Fann: {
+      Aspirant: "https://google.com",
+      Many:"https://google.com"
+    }
+  },
+  Mage: {
+    Nana: {
+      Aspirant: "https://google.com",
+      Many:"https://google.com"
+    },
+    Eudora: {
+      Aspirant: "https://google.com",
+      Many:"https://google.com"
+    }
+  }
+};
