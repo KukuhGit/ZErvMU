77df932b1a5c91f7945d24c8f5bd2637660ac750
KukuhGit <97213948+KukuhGit@users.noreply.github.com>
2023-05-24 22:26:24 +0700

    urls.js

--- urls.js
+++ urls.js
@@ -1,23 +1,43 @@
-  
+  let a="https://github.com/lanz132/ASSASSIN/raw/main/",
+      m="https://github.com/lanz132/ASSASSIN/raw/main/",
+      k="https://github.com/lanz132/ASSASSIN/raw/main/",
+      f="https://github.com/lanz132/ASSASSIN/raw/main/",
+      t="https://github.com/lanz132/ASSASSIN/raw/main/",
+      s="https://github.com/lanz132/ASSASSIN/raw/main/";
     var obj = {
   Assassin: {
-    Fanny: {
-      Aspirant: "https://google.com",
-      Many:"https://google.com"
+    Aamon: {
+        Elite:a+"AAMON ELITE.zip",
+        Starlight:a+"AAMON STARLIGHT.zip"
+      },
+    Benedeta: {
+        Special:a+"BENEDETTA SPECIAL.zip"
     },
-    Fann: {
-      Aspirant: "https://google.com",
-      Many:"https://google.com"
-    }
+    Fanny: {
+      Aspirant:a+"FANNY ASPIRANT.zip",
+      Christmas:a+"FANNY CHRISTMAS.zip",
+      "Emperial Warrior":a+"FANNY EMPERIAL WARRIOR.zip"
+    },
+    Hayabusa: {
+      Backup:a+"HAYABUSA BACKUP.zip",
+      "11.11":a+"HAYABUSA 11.11.zip",
+      Elite:a+"HAYABUSA ELITE.zip",
+      Epic:a+"HAYABUSA EPIC.zip"
+    },
   },
   Mage: {
-    Nana: {
-      Aspirant: "https://google.com",
-      Many:"https://google.com"
-    },
-    Eudora: {
-      Aspirant: "https://google.com",
-      Many:"https://google.com"
-    }
+    
+  },
+  Marksman: {
+    
+  },
+  Fighter: {
+  
+  },
+  Tank: {
+    
+  },
+  Support: {
+    
   }
 };
